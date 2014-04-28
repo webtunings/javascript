1a84e1e8903e7ad378e58895e9f4a1dae28b0d5d
webtunings <webtunings@gmail.com>
2014-04-28 20:50:16 +0530

    Babylonian method for square root

--- sqrt.js
+++ sqrt.js
@@ -2,7 +2,6 @@ function sqrt(number) {
  var start = number;
  var error = 0.00001;
 
-
  while((start - number/start) > error) {
  start = (start + number/start)/2;
  console.log('intermediate result= ' + start); 
